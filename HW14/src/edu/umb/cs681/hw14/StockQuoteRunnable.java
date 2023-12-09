5f0c4643edb22434aabad88f5daa9f6c4a2dd68e
VishwajithP2308 <113051064+VishwajithP2308@users.noreply.github.com>
2023-12-08 19:03:45 -0500

    HW14

---
+++ HW14/src/edu/umb/cs681/hw14/StockQuoteRunnable.java
@@ -0,0 +1,33 @@
+package edu.umb.cs681.hw14;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class StockQuoteRunnable implements Runnable{
+    private AtomicBoolean done = new AtomicBoolean(false);
+    public void setDone(){
+        done.getAndSet(true);
+    }
+    @Override
+    public void run() {
+        while (true){
+            if (done.get()){
+                System.out.println("Flag based interruption triggered");
+                break;
+            }
+            StockQuoteObservable stockQuoteObservable = new StockQuoteObservable();
+            String ticker = "Ticker" + Math.random();
+            double newQuote = 20.0 + Math.random() * 10;
+            stockQuoteObservable.addObserver(new LineChartObserver());
+            stockQuoteObservable.addObserver(new TableObserver());
+            stockQuoteObservable.addObserver(new ThreeDObserver());
+            stockQuoteObservable.changeQuote(ticker, newQuote);
+            System.out.println(stockQuoteObservable.getMap());
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException ex){
+                System.out.println("Interruption based termination triggered");
+            }
+        }
+
+    }
+}
