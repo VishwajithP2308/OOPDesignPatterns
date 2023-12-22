102b488d4451ca40b022d8b5b2df3c990f486825
VishwajithP2308 <113051064+VishwajithP2308@users.noreply.github.com>
2023-12-21 21:36:08 -0500

    Delete HW15 directory

--- HW15/src/edu/umb/cs681/hw15/Observer.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs681.hw15;
-
-public interface Observer<T> {
-	public void update(Observable<T> sender, T event);
-}
--- HW15/src/edu/umb/cs681/hw15/StockEvent.java
+++
@@ -1,3 +0,0 @@
-package edu.umb.cs681.hw15;
-public record StockEvent(String ticker, double quote) {
-}
