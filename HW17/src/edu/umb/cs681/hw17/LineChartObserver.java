002aeab487d2e60c25d0fa249a814567b8686fac
VishwajithP2308 <113051064+VishwajithP2308@users.noreply.github.com>
2023-12-21 23:08:11 -0500

    HW17

---
+++ HW17/src/edu/umb/cs681/hw17/LineChartObserver.java
@@ -0,0 +1,16 @@
+package edu.umb.cs681.hw17;
+
+public class LineChartObserver implements Observer<StockEvent>{
+
+	@Override
+	public void update(Observable<StockEvent> sender, StockEvent event) {
+		String ticker = event.ticker();
+        String quote = String.valueOf(event.quote());
+        System.out.println("Line Chart Observer...\n");
+        System.out.println("Ticker = " + ticker + "\nQuote = " + quote + "\n");
+        System.out.println("------------------------------------------------\n");
+		
+	}
+	
+	
+}
